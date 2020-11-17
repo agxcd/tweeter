e93a432873cf7fd4c84d78a6b5ac9837bea13dc0
agxcd <agxcdcode@gmail.com>
2020-11-17 08:17:53 +0000

    adding the wordcount js

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  console.log("Hello");
+});
+
+$("#tweet-text").on("keyup", function () {
+  let remaining = 140 - $(this).val().length;
+  const counter = $("#counter");
+  counter.text(remaining);
+  if (remaining > 10) {
+    counter.removeClass("warning");
+  } else if (remaining <= 10) {
+    counter.addClass("warning");
+  }
+});
