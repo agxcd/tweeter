dea0cb7f06d3af819c05457f72ce5e5d6d3153ab
agxcd <agxcdcode@gmail.com>
2020-11-19 04:29:39 +0000

    delete local data object in client.js fetching data from http://localhost:8080/tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,30 +1,3 @@
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    user: {
-      name: "Newton",
-      avatars: "https://i.imgur.com/73hZDYK.png",
-      handle: "@SirIsaac",
-    },
-    content: {
-      text:
-        "If I have seen further it is by standing on the shoulders of giants",
-    },
-    created_at: 1461116232227,
-  },
-  {
-    user: {
-      name: "Descartes",
-      avatars: "https://i.imgur.com/nlhLi3I.png",
-      handle: "@rd",
-    },
-    content: {
-      text: "Je pense , donc je suis",
-    },
-    created_at: 1461113959088,
-  },
-];
-
 $(document).ready(function () {
   const renderTweets = function (tweets) {
     return tweets.map((tweet) =>
@@ -56,7 +29,6 @@ $(document).ready(function () {
     `;
     return $tweet;
   };
-  // renderTweets(data);
 
   $("form").submit(function (event) {
     console.log($(this).serialize());
