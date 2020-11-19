8d4a3a7cfb85bdc8d2829b4761531644a5763b21
agxcd <agxcdcode@gmail.com>
2020-11-19 01:42:10 +0000

    Form Submission using JQuery

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,31 +1,31 @@
 // Fake data taken from initial-tweets.json
-$(document).ready(function () {
-  const data = [
-    {
-      user: {
-        name: "Newton",
-        avatars: "https://i.imgur.com/73hZDYK.png",
-        handle: "@SirIsaac",
-      },
-      content: {
-        text:
-          "If I have seen further it is by standing on the shoulders of giants",
-      },
-      created_at: 1461116232227,
+const data = [
+  {
+    user: {
+      name: "Newton",
+      avatars: "https://i.imgur.com/73hZDYK.png",
+      handle: "@SirIsaac",
     },
-    {
-      user: {
-        name: "Descartes",
-        avatars: "https://i.imgur.com/nlhLi3I.png",
-        handle: "@rd",
-      },
-      content: {
-        text: "Je pense , donc je suis",
-      },
-      created_at: 1461113959088,
+    content: {
+      text:
+        "If I have seen further it is by standing on the shoulders of giants",
     },
-  ];
+    created_at: 1461116232227,
+  },
+  {
+    user: {
+      name: "Descartes",
+      avatars: "https://i.imgur.com/nlhLi3I.png",
+      handle: "@rd",
+    },
+    content: {
+      text: "Je pense , donc je suis",
+    },
+    created_at: 1461113959088,
+  },
+];
 
+$(document).ready(function () {
   const renderTweets = function (tweets) {
     return tweets.map((tweet) =>
       $(".tweets-container").append(createTweetElement(tweet))
@@ -56,6 +56,11 @@ $(document).ready(function () {
     `;
     return $tweet;
   };
-
   renderTweets(data);
+});
+
+$("form").submit(function (event) {
+  console.log($(this).serialize());
+  alert($(this).serialize());
+  event.preventDefault();
 });
