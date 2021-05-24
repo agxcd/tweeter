5bf5ee6f2bfb562678e124d73459f1cde9f70f32
agxcd <agxcdcode@gmail.com>
2021-05-23 21:03:50 -0700

    minor change on styles

--- server/index.js
+++ server/index.js
@@ -7,9 +7,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 app.use(express.static("public"));
 
 // The in-memory database of tweets. It's a basic object with an array in it.
@@ -35,5 +37,5 @@ const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 app.use("/tweets", tweetsRoutes);
 
 app.listen(PORT, () => {
-  console.log("Example app listening on port " + PORT);
+  console.log("Example app listening on port http://localhost:" + PORT + "/");
 });
